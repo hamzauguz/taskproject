e17eb9ca1c000398cad10a1813ed019a192a4600
hamza <arastiranhamza@gmail.com>
2023-06-22 14:19:41 +0300

    footer a başlangıç yapıldı

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from "react";
 import "./App.css";
 import Header from "./components/header";
+import Footer from "./components/footer";
 
 const App = () => {
   return (
     <div className="App">
       <Header />
       nasıl gidiyor
+      <Footer />
     </div>
   );
 };
---
+++ src/components/footer/index.js
@@ -0,0 +1,8 @@
+import React from "react";
+import "./Styles.footer.css";
+
+const Footer = () => {
+  return <footer>Footer</footer>;
+};
+
+export default Footer;
