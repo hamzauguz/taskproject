06e9f452180fbb7bd0d5b36ef01396198d31f590
hamza <arastiranhamza@gmail.com>
2023-06-23 11:22:00 +0300

    footer dimension yapısı eklenerek tamamlandı

--- src/components/footer/index.js
+++ src/components/footer/index.js
@@ -2,7 +2,42 @@ import React from "react";
 import "./Styles.footer.css";
 
 const Footer = () => {
-  return <footer>Footer</footer>;
+  return (
+    <footer className="footerContainer">
+      <div className="footerDivContainer">
+        <div className="leftFooterPlace">
+          <span className="footerTitle">Let there be change</span>
+          <div className="gridFooterPages">
+            <a className="footerPageItem">Preference Center</a>
+            <a className="footerPageItem">Privacy Statement</a>
+            <a className="footerPageItem">Careers</a>
+            <a className="footerPageItem">Terms & Conditions</a>
+
+            <a className="footerPageItem">About Us</a>
+            <a className="footerPageItem">Cookie Policy/Settings</a>
+
+            <a className="footerPageItem">Contact Us</a>
+            <a className="footerPageItem">Accessibility Statement</a>
+
+            <a className="footerPageItem">Locations</a>
+            <a className="footerPageItem">
+              Do Not Sell/Share My Personal Information (for CA)
+            </a>
+            <a className="footerPageItem lastFooterTitle">Sitemap</a>
+
+            {/* <div>left right div</div> */}
+          </div>
+          <span className="footerSignTitle">
+            © 2023 Accenture. All Rights Reserved.
+          </span>
+        </div>
+        <img
+          className="footerImageRight"
+          src={require("../../assets/footer.png")}
+        />
+      </div>
+    </footer>
+  );
 };
 
 export default Footer;
